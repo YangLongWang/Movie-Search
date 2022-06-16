84405079d616b936a4fad828a061e6e572c9a1d6
RubinoD1 <Rubinod1@gmail.com>
2022-06-15 21:06:45 -0400

    getting response.ok from api and consol.log data

--- assets/js/watchapi.js
+++ assets/js/watchapi.js
@@ -1,7 +1,26 @@
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'cfff8e47bbmsh52112d53558a8c6p18b44djsn557c06239ea5',
+		'X-RapidAPI-Host': 'streaming-availability.p.rapidapi.com'
+	}
+};
+
+
 var getWatchApi = function(){ 
-    var response= fetch("https://api.github.com/users/octocat/repos");
+    var response= fetch('https://streaming-availability.p.rapidapi.com/search/basic?country=us&service=netflix&type=movie&genre=18&page=1&output_language=en&language=en', options).then(response => response.json())
     console.log(response);
-    //console.log("I'm alive!")
 };
 
 getWatchApi();
+
+
+
+
+//reference data
+/*
+fetch('https://streaming-availability.p.rapidapi.com/search/basic?country=us&service=netflix&type=movie&genre=18&page=1&output_language=en&language=en', options)
+	.then(response => response.json())
+	.then(response => console.log(response))
+	.catch(err => console.error(err));
+*/
