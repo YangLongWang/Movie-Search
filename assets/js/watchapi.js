c41138c2e26c95ad375ba4ee6b947f971ef12a76
RubinoD1 <Rubinod1@gmail.com>
2022-06-15 22:50:01 -0400

    added search function and some doms

--- assets/js/watchapi.js
+++ assets/js/watchapi.js
@@ -1,3 +1,8 @@
+//DOM 
+var movieformEl = document.getElementById("#movie-form");
+var movieInputEl = document.getElementById("#search-input");
+
+//Array that holds api key and other verification
 const options = {
 	method: 'GET',
 	headers: {
@@ -7,19 +12,23 @@ const options = {
 };
 
 
+
 var getWatchApi = function(){ 
     var response= fetch('https://streaming-availability.p.rapidapi.com/search/basic?country=us&service=netflix&type=movie&genre=18&page=1&output_language=en&language=en', options).then(function(response) {
         response.json().then(function(data) {
             console.log(data);
         });
-       
-    }
-   
-)};
+       }
+   )};
 
-getWatchApi();
+//getWatchApi();
 
 
+//search function
+var movieSearchHandler = function(event) {
+    event.preventDefault();
+    console.log(event);
+}
 
 
 //reference data
