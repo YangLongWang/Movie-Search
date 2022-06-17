fe332fcc579f221b80a9a2aaca04536b0e048ee6
Enzo Tragnone <enzo.tragnone@gmail.com>
2022-06-17 19:34:30 -0400

    merge 1 of both JS files. Not yet finished

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,6 +1,12 @@
 //variables declaration
 key2 = "cfff8e47bbmsh52112d53558a8c6p18b44djsn557c06239ea5";
 let btn = document.querySelector(".button");
+var movieformEl = document.querySelector("#movie-form");
+var movieInputEl = document.querySelector("#search-input");
+var movieTitleEl = document.querySelector("#movie_title");
+var movieInfoEl = document.querySelector("#synopsis");
+var movieLengthEl = document.querySelector("#length");
+var movieDateEl = document.querySelector("#date");
 // info(184126)
 
 //IP adress look up API
@@ -15,27 +21,8 @@ let btn = document.querySelector(".button");
 //         return location;
 //     });
 
-//function for live search
-function search(input) {
-    let apiKey = '627ce180f6b942d38cd09ef7905db024';
-    fetch('https://ipgeolocation.abstractapi.com/v1/?api_key=' + apiKey)
-        .then(response => response.json())
-        .then(data => {
-            console.log((data))
-            inputPass = input
-            lat = data.latitude.toString();
-            long = data.longitude.toString();
-            var today = new Date();
-            var date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
-            var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds() + ":" + today.getMilliseconds();
-            var dateTime = date + 'T' + time + "Z";
-            console.log(dateTime);
-            let location = lat + ";" + long;
-            moviesearch(inputPass, location, dateTime)
-        });
-};
 
-
+//function to look up IMDB ID
 function idSearch(input) {
     const options = {
         method: 'GET',
@@ -52,9 +39,11 @@ function idSearch(input) {
             topId = data.results[0].id.slice(7, 16);
             topTitle = data.results[0].title;
             MovieInfo(topId);
+            getWatchApi(topId);
         })
 }
 
+//function for movie info
 function MovieInfo(id) {
     const options = {
         method: 'GET',
@@ -71,7 +60,63 @@ function MovieInfo(id) {
         })
 }
 
-btn.addEventListener("click", function () {
-    UserInput = document.querySelector("input").value;
-    idSearch(UserInput);
-})
+//Array that holds api key and other verification
+const options = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': 'cfff8e47bbmsh52112d53558a8c6p18b44djsn557c06239ea5',
+        'X-RapidAPI-Host': 'streaming-availability.p.rapidapi.com'
+    }
+};
+
+//data value is set to movieTerm and used in var displayWatchInfo
+let movieTerm = '';
+
+//Makes fetch call 
+var getWatchApi = function (movie) {
+    fetch("https://streaming-availability.p.rapidapi.com/search/basic?country=us&service=netflix&type=movie&keyword=" + movie + "&page=1&output_language=en&language=en", options).then(function (response) {
+        response.json().then(function (data) {
+            console.log(data);
+            movieTerm = data;
+            displayWatchInfo(movieTerm);
+        });
+    }
+    )
+};
+
+//display data
+var displayWatchInfo = function (movieTerm) {
+    //console.log(movieTerm.results[0].title);
+    movieTitleEl.textContent = movieTerm.results[0].title;
+    movieInfoEl.textContent = movieTerm.results[0].overview;
+    movieLengthEl.textContent = movieTerm.results[0].runtime;
+    movieDateEl.textContent = movieTerm.results[0].year;
+};
+
+
+//event handler function
+var movieSearchHandler = function (event) {
+    event.preventDefault();
+    //get value from input element and eliminates extra spaces 
+    var movie = movieInputEl.value.trim();
+    if (movie) {
+        idSearch(movie);
+        movieInputEl.value = "";
+    } else {
+        //**can we use alerts/prompts for this project?** 
+        alert("Please enter a movie name");
+    }
+};
+
+//event handler function call
+movieformEl.addEventListener("submit", movieSearchHandler);
+
+
+
+//reference data
+/*
+fetch('https://streaming-availability.p.rapidapi.com/search/basic?country=us&service=netflix&type=movie&genre=18&page=1&output_language=en&language=en', options)
+    .then(response => response.json())
+    .then(response => console.log(response))
+    .catch(err => console.error(err));
+*/
