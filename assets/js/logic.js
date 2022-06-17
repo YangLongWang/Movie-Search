035d7453f2389828d9b66e4ba1c20059e7224a8d
Enzo Tragnone <enzo.tragnone@gmail.com>
2022-06-17 18:29:31 -0400

    change of API for main movie info, addition of event handler

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,62 +1,77 @@
-Key1 = "vJ66865rhP2CnGnuEHAg22qFVMVD26YJ4CcdT11v";
+//variables declaration
 key2 = "cfff8e47bbmsh52112d53558a8c6p18b44djsn557c06239ea5";
-info(184126)
-
-
 let btn = document.querySelector(".button");
+// info(184126)
 
+//IP adress look up API
+// let apiKey = '627ce180f6b942d38cd09ef7905db024';
+// fetch('https://ipgeolocation.abstractapi.com/v1/?api_key=' + apiKey)
+//     .then(response => response.json())
+//     .then(data => {
+//         console.log((data))
+//         lat = data.latitude.toString();
+//         long = data.longitude.toString();
+//         let location = lat + ";" + long;
+//         return location;
+//     });
 
-
-
-btn.addEventListener("click", function () {
+//function for live search
+function search(input) {
     let apiKey = '627ce180f6b942d38cd09ef7905db024';
     fetch('https://ipgeolocation.abstractapi.com/v1/?api_key=' + apiKey)
         .then(response => response.json())
         .then(data => {
             console.log((data))
+            inputPass = input
             lat = data.latitude.toString();
             long = data.longitude.toString();
+            var today = new Date();
+            var date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+            var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds() + ":" + today.getMilliseconds();
+            var dateTime = date + 'T' + time + "Z";
+            console.log(dateTime);
             let location = lat + ";" + long;
+            moviesearch(inputPass, location, dateTime)
         });
+};
+
+
+function idSearch(input) {
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': 'cfff8e47bbmsh52112d53558a8c6p18b44djsn557c06239ea5',
+            'X-RapidAPI-Host': 'online-movie-database.p.rapidapi.com'
+        }
+    };
+
+    fetch('https://online-movie-database.p.rapidapi.com/title/find?q=' + input + '', options)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            topId = data.results[0].id.slice(7, 16);
+            topTitle = data.results[0].title;
+            MovieInfo(topId);
+        })
+}
+
+function MovieInfo(id) {
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': 'cfff8e47bbmsh52112d53558a8c6p18b44djsn557c06239ea5',
+            'X-RapidAPI-Host': 'online-movie-database.p.rapidapi.com'
+        }
+    };
+
+    fetch('https://online-movie-database.p.rapidapi.com/title/get-overview-details?tconst=' + id + '&currentCountry=CA', options)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+        })
+}
+
+btn.addEventListener("click", function () {
+    UserInput = document.querySelector("input").value;
+    idSearch(UserInput);
 })
-
-function info(location) {
-    info = document.querySelector("input").value;
-    now = getTime();
-    console.log(now);
-    fetch("https://api-gate2.movieglu.com/filmLiveSearch/?query=" + info + "&n=6", {
-        headers: {
-            "Api-Version": "v200",
-            "Authorization": "Basic U1RVRF8yMjM6Mk9sVEhKMGJjbElO",
-            "Client": "STUD_223",
-            "location": location,
-            "Device-Datetime": "2022-06-16T18:38:24.857Z",
-            "Territory": "CA",
-            "X-Api-Key": "vJ66865rhP2CnGnuEHAg22qFVMVD26YJ4CcdT11v"
-        }
-    }).then(function (response) {
-        response.json().then(function (data) {
-            console.log(data);
-        })
-    });
-}
-
-//test with this
-function info(id) {
-    fetch("https://api-gate2.movieglu.com/filmDetails/?film_id=" + id + "", {
-        headers: {
-            "Api-Version": "v200",
-            "Authorization": "Basic U1RVRF8yMjNfWFg6MEM4S3k3UnQ2V0pL",
-            "Client": "STUD_223",
-            "Device-Datetime": "2022-06-16T18:38:24.857Z",
-            "Territory": "XX",
-            "X-Api-Key": "PEmYjxkTo66M2GZ8vnyNF6J876smHZ8m3T0jUAfH"
-        }
-    }).then(function (response) {
-        response.json().then(function (data) {
-            console.log(data);
-        })
-    });
-}
-
-184126
