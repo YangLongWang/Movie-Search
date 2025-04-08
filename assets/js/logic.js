588b11aaae58fe62f0ea68e1be841978b147278f
yang <joey674901205@gmail.com>
2025-04-09 01:48:32 +0800

    successfully removed the input value and movie info, make sure it's new.

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,24 +1,19 @@
 // API Read Access Token:eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwOTRjODRkYjU5N2RlYjQ5OGY4ZDkwYTI0NzQ1MTNmZSIsIm5iZiI6MTY1NTU4MDU1Mi44MzgsInN1YiI6IjYyYWUyNzg4YTZhNGMxMDBmMGFhYWNkYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.srm6rQi95Et7kAUp69oL7NFBB8ornMEwyab1kqgSdD4
 // let APIKey = '094c84db597deb498f8d90a2474513fe';
-// curl --request GET \
-// --url 'https://api.themoviedb.org/3/movie/11' \
-// --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwOTRjODRkYjU5N2RlYjQ5OGY4ZDkwYTI0NzQ1MTNmZSIsIm5iZiI6MTY1NTU4MDU1Mi44MzgsInN1YiI6IjYyYWUyNzg4YTZhNGMxMDBmMGFhYWNkYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.srm6rQi95Et7kAUp69oL7NFBB8ornMEwyab1kqgSdD4'
 
 //variables declaration
 const nowShowingDate = document.getElementById("now-showing-date");
 const loadImg = document.getElementById("loadImg");
 const topRated = document.getElementById("top-rated");
 const searchButton = document.getElementById("searchButton");
-let searchText = document.getElementById("searchText");
-// const Poster_Name = document.querySelector("#top-rated");
-// console.log(Poster_Name)
+const nowShowingPart = document.getElementById('now-showing-part');
+const moreMoviesPart = document.getElementById('more-movies-part');
+const movies = document.getElementById('movies');
+const contactUs = document.getElementById('contactUs');
 
-// logic
-// async function getText(file) {
-//     let myObject = await fetch(file);
-//     let myText = await myObject.text();
-//     myDisplay(myText);
-// }
+
+// const Poster_Name = document.querySelector("#top-rated");
+
 
 const options = {
     method: 'GET',
@@ -66,10 +61,8 @@ fetch('https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1', op
 fetch('https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1', options)
     .then(res => res.json())
     .then(data => {
-        // console.log(data)
         if (data.results) {
             let movieArr = data.results;
-            // console.log(movieArr);
             for (let i = 0; i < movieArr.length; i++) {
                 let poster = movieArr[i].poster_path;
                 let title = movieArr[i].original_title;
@@ -97,9 +90,12 @@ fetch('https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1', opti
     })
     .catch(err => console.error(err));
 
-//click one movie poster
+// click one movie poster
+// 点击海报或电影名可以显示该电影信息 需要完成
 function clickPoster(event) {
-    // console.log(event.target)
+    event.preventDefault();
+
+    console.log(event.target)
     if (event.target) {
         const title1 = event.target["alt"];
         const title2 = event.target.innerHTML;
@@ -108,22 +104,21 @@ function clickPoster(event) {
     }
 }
 
+// search/movie (more)
 function searchFunction(event) {
     event.preventDefault();
 
     let searchText = document.getElementById("searchText").value;
-    // search/movie (more)
     fetch('https://api.themoviedb.org/3/search/movie?query=' + searchText + '&include_adult=false&language=en-US&page=1', options)
         .then(res => res.json())
         .then(data => {
             if (data.results) {
                 let movieArr = data.results;
-                let container = document.getElementById('container');
                 for (let i = 0; i < movieArr.length; i++) {
                     // div->class="movie-info"
                     let movieInfo = document.createElement("div");
                     movieInfo.classList.add('movie-info');
-                    container.appendChild(movieInfo);
+                    movies.appendChild(movieInfo);
                     // h2->class="text-center"
                     let title = document.createElement('h2');
                     title.classList.add('text-center');
@@ -174,28 +169,19 @@ function searchFunction(event) {
             }
         })
         .catch(err => console.error(err));
+    document.getElementById('searchText').value = '';
 };
 
+// remove parts
 function cleanFunction() {
-    // remove parts
-    let r1 = document.querySelector('.now-showing-part');
-    if (r1) {
-        r1.remove();
+    nowShowingPart.remove();
+    moreMoviesPart.remove();
+    contactUs.remove();
+    while (movies.hasChildNodes()) {
+        movies.removeChild(movies.childNodes[0])
     }
-    let r2 = document.querySelector('.more-movies-part');
-    if (r2) {
-        r2.remove();
-    }
-    let r3 = document.querySelector('.contactUs');
-    // r3.style = 'visibility:hidden';
-    if (r3) {
-        r3.remove();
-    }
-
-
-
 }
 
 searchButton.addEventListener('click', searchFunction);
 searchButton.addEventListener('click', cleanFunction);
-// Poster_Name.addEventListener('click', clickPoster);
+
